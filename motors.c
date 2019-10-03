95bea285ce205cd82ead1fb65c910ccbf7f22b58
Daniel Axelsson <Daniel.Axelsson@yh.nackademin.se>
2019-10-03 10:45:32 +0200

    Several motor funktions and definitions written
    Change-Id: I4646c5719d0f6f1424977a3100155da5a8190f3a

--- motors.c
+++ motors.c
@@ -11,17 +11,14 @@ void motors_init()
 	TCCR2A |= (1 << COM2A1) | (1 << COM2B1) | (1 << WGM22) | (1 << WGM21) | (1 << WGM20);
 	TCCR2B |= (1 << CS21) | (1 << CS20);
 	TCCR2B &= ~(1 << CS22);
-
-	//pin 3; PWM A OCR2A
-	//pin 11; PWM B OCR2B
 }
 
-void motor_control(uint8_t motor, uint8_t direction, uint8_t speed)
+void motor_single_control(uint8_t motor, uint8_t direction, uint8_t speed)
 {
 	if (motor == MOTOR_A)
 	{
 		printf("Motor A\n");
-		if (direction == 1)
+		if (direction == MOTOR_A_FORWARD)
 		{
 			PORTB |= (1 << PORTB4);
 		}
@@ -35,7 +32,7 @@ void motor_control(uint8_t motor, uint8_t direction, uint8_t speed)
 	else if (motor == MOTOR_B)
 	{
 		printf("Motor B\n");
-		if (direction == 1)
+		if (direction == MOTOR_B_FORWARD)
 		{
 			PORTB |= (1 << PORTB5);
 		}
@@ -48,24 +45,55 @@ void motor_control(uint8_t motor, uint8_t direction, uint8_t speed)
 	}
 }
 
-void motor_forward(uint8_t speed)
+void motor_straight(uint8_t direction, uint8_t speed)
 {
+	if (direction == MOTORS_FORWARD)
+	{
+		PORTB |= (1 << PORTB4);
+		PORTB &= ~(1 << PORTB5);
+	}
+	else
+	{
+		PORTB &= ~(1 << PORTB4);
+		PORTB |= (1 << PORTB5);
+	}
 
+	OCR2A = speed;
+	OCR2B = speed;
 }
 
-void motor_backwards(uint8_t speed)
+void motor_turn(uint8_t direction, uint8_t speed_A, uint8_t speed_B)
 {
+	if (direction == MOTORS_FORWARD)
+	{
+		PORTB |= (1 << PORTB4);
+		PORTB &= ~(1 << PORTB5);
+	}
+	else
+	{
+		PORTB &= ~(1 << PORTB4);
+		PORTB |= (1 << PORTB5);
+	}
 
+	OCR2A = speed_A;
+	OCR2B = speed_B;
 }
 
-void motor_turn(uint8_t speed_A, uint8_t speed_B)
+void motor_spin(uint8_t rotating_direction, uint8_t speed)
 {
+	if (rotating_direction == MOTORS_CLOCKWISE)
+	{
+		PORTB |= (1 << PORTB4);
+		PORTB |= (1 << PORTB5);
+	}
+	else
+	{
+		PORTB &= ~(1 << PORTB4);
+		PORTB &= ~(1 << PORTB5);
+	}
 
-}
-
-void motor_spin(uint8_t direction, uint8_t speed)
-{
-
+	OCR2A = speed;
+	OCR2B = speed;
 }
 
 void motor_full_stop()
--- motors.h
+++ motors.h
@@ -1,21 +1,26 @@
 #ifndef _MOTORS_H_
 #define _MOTORS_H
 
+#define MOTORS_BACKWARD 0
+#define MOTORS_FORWARD 1
+#define MOTORS_CLOCKWISE 0
+#define MOTORS_COUNTER_CLOCKWISE 1
+
 #define MOTOR_A 0
 #define MOTOR_B 1
-
-#define BACKWARD 0
-#define FORWARD 1
+//Riktning för motor B inverterad pga. motorlödning
+#define MOTOR_A_BACKWARD 0
+#define MOTOR_A_FORWARD 1
+#define MOTOR_B_BACKWARD 1
+#define MOTOR_B_FORWARD 0
 
 void motors_init();
 
-void motor_control(uint8_t motor, uint8_t direction, uint8_t speed);
+void motor_single_control(uint8_t motor, uint8_t direction, uint8_t speed);
 
-void motor_forward(uint8_t speed);
+void motor_straight(uint8_t direction, uint8_t speed);
 
-void motor_backwards(uint8_t speed);
-
-void motor_turn(uint8_t speed_A, uint8_t speed_B);
+void motor_turn(uint8_t direction, uint8_t speed_A, uint8_t speed_B);
 
 void motor_spin(uint8_t direction, uint8_t speed);
 
