898287010a88ae5beb5b4a6306a99144107052e5
Love Berg <love.berg@yh.nackademin.se>
2019-10-07 13:49:07 +0200

    linesensor fungerar nästan!

--- linesensor.c
+++ linesensor.c
@@ -45,8 +45,8 @@ void linesens_charge(){
 
   linesens_10us_delay();
 
-  PORTD &= ~(1 << PORTD4) | (1 << PORTD5) | (1 << PORTD6) | (1 << PORTD7);
-  PORTB &= ~(1 << PORTB0) | (1 << PORTB1);
+  PORTD &= ~((1 << PORTD4) | (1 << PORTD5) | (1 << PORTD6) | (1 << PORTD7));
+  PORTB &= ~((1 << PORTB0) | (1 << PORTB1));
 }
 
 
@@ -58,28 +58,30 @@ void linesens_read(){
 
 
   linesens_charge();
+
+  DDRD &= ~((1 << DDD4) | (1 << DDD5) | (1 << DDD6) | (1 << DDD7));
+  DDRB &= ~((1 << DDB0) | (1 << DDB1)); //sets pins to inputs
+
+
   TCCR0B |= (1 << CS02); //starts 2500us timer
 
-  DDRD &= ~(1 << DDD4) | (1 << DDD5) | (1 << DDD6) | (1 << DDD7);
-  DDRB &= ~(1 << DDB0) | (1 << DDB1); //sets pins to inputs
-
   while (!(TIFR0 & (1 << OCF0A))){
-    if (!(PINB & (1 << PINB0)) && sensorValues[0] == 0){
+    if (!(PINB & (1 << PINB1)) && sensorValues[0] == 0){
         sensorValues[0] = TCNT0;
       }
-     if (!(PINB & (1 << PINB1)) && sensorValues[1] == 0){
+     if (!(PINB & (1 << PINB0)) && sensorValues[1] == 0){
          sensorValues[1] = TCNT0;
        }
-     if (!(PIND & (1 << PIND4)) && sensorValues[2] == 0){
+     if (!(PIND & (1 << PIND7)) && sensorValues[2] == 0){
          sensorValues[2] = TCNT0;
        }
-     if (!(PIND & (1 << PIND5)) && sensorValues[3] == 0){
+     if (!(PIND & (1 << PIND6)) && sensorValues[3] == 0){
          sensorValues[3] = TCNT0;
        }
-     if (!(PIND & (1 << PIND6)) && sensorValues[4] == 0){
+     if (!(PIND & (1 << PIND5)) && sensorValues[4] == 0){
          sensorValues[4] = TCNT0;
        }
-     if (!(PIND & (1 << PIND7)) && sensorValues[5] == 0){
+     if (!(PIND & (1 << PIND4)) && sensorValues[5] == 0){
          sensorValues[5] = TCNT0;
        }
   }
